daca34c57cf588dd88e736366a5bd26a6cb3b38b
HendhyW <hendhywirawan@gmail.com>
2025-04-27 12:39:20 +0700

    chall 2 finished

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (count % 10 === 0) {
+      alert( count.toString() + ' dapat dibagi dengan 10');
+    }
+  }, [count]);
+
+  return (
+    <> 
+      <h1 className="text-3xl font-bold p-10">
+        Counter Demo
+      </h1>
+      <h1 className='text-3xl pb-10'>
+        {count}
+      </h1>
+      <div className="space-x-6">
+        <button onClick={() => setCount((count) => count - 1)} className="">
+          -
+        </button>
+        <button onClick={() => setCount((count) => 0)} className=''>
+          reset
+        </button>
+        <button onClick={() => setCount((count) => count + 1)} className=''>
+          +
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
